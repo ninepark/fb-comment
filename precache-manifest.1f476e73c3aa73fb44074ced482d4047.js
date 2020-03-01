5ac381faa7a53a0cfc620aa0e3e0a1f297cf97a4
ninepark <hoophooop111@gmail.com>
2020-03-01 14:02:27 +0900

    Updates

---
+++ precache-manifest.1f476e73c3aa73fb44074ced482d4047.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c698f40fc56237ef84104848b3b54a5",
+    "url": "/fb-comment/index.html"
+  },
+  {
+    "revision": "39d29b66cf25a93d576f",
+    "url": "/fb-comment/static/css/main.29d29e65.chunk.css"
+  },
+  {
+    "revision": "4062fdf5e55675df048f",
+    "url": "/fb-comment/static/js/2.ca043fee.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/fb-comment/static/js/2.ca043fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39d29b66cf25a93d576f",
+    "url": "/fb-comment/static/js/main.c92c4b4c.chunk.js"
+  },
+  {
+    "revision": "4f8fc8b38cd442277abe",
+    "url": "/fb-comment/static/js/runtime-main.d49cbaea.js"
+  },
+  {
+    "revision": "4cb5b4834abc50ab58cf68449d8b8426",
+    "url": "/fb-comment/static/media/avatar.4cb5b483.svg"
+  },
+  {
+    "revision": "698fcf50960249bbac5bedbc6035d874",
+    "url": "/fb-comment/static/media/comment.698fcf50.svg"
+  },
+  {
+    "revision": "a78d2be110fabc33946c129df8e2742c",
+    "url": "/fb-comment/static/media/theEyeOfDarkness.a78d2be1.jpg"
+  },
+  {
+    "revision": "61b20242d944933456b70c8bcc534168",
+    "url": "/fb-comment/static/media/thumbup.61b20242.svg"
+  }
+]);
